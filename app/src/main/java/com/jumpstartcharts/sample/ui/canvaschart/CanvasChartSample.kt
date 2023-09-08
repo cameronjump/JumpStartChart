4d00fc69d3d78319a36d44c8cb7442a76751f299
cameronjump <cjump@fundrise.com>
2023-09-08 17:57:31 -0600

    feature: fix quadratic sample

--- app/src/main/java/com/jumpstartcharts/sample/ui/canvaschart/CanvasChartSample.kt
+++ app/src/main/java/com/jumpstartcharts/sample/ui/canvaschart/CanvasChartSample.kt
@@ -1,6 +1,7 @@
 package com.jumpstartcharts.sample.ui.canvaschart
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,7 +15,7 @@ import androidx.compose.ui.unit.dp
 @Preview(showBackground = true)
 @Composable
 fun CanvasChartStepOne() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
         // ...
     }
 }
@@ -22,13 +23,15 @@ fun CanvasChartStepOne() {
 @Preview(showBackground = true)
 @Composable
 fun CanvasChartStepTwo() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
         // draw x-axis
         drawLine(
             color = Color.Black,
             start = Offset(x = 0f, y = size.height / 2f),
             end = Offset(x = size.width, y = size.height / 2f),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
     }
 }
@@ -36,13 +39,15 @@ fun CanvasChartStepTwo() {
 @Preview(showBackground = true)
 @Composable
 fun CanvasChartStepThree() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
         // draw x-axis
         drawLine(
             color = Color.Black,
             start = Offset(x = 0f, y = size.height / 2f),
             end = Offset(x = size.width, y = size.height / 2f),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
         // draw y-axis
@@ -50,7 +55,7 @@ fun CanvasChartStepThree() {
             color = Color.Black,
             start = Offset(x = size.width / 2f, y = 0f),
             end = Offset(x = size.width / 2f, y = size.height),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
     }
 }
@@ -58,13 +63,15 @@ fun CanvasChartStepThree() {
 @Preview(showBackground = true)
 @Composable
 fun CanvasChartStepFour() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
         // draw x-axis
         drawLine(
             color = Color.Black,
             start = Offset(x = 0f, y = size.height / 2f),
             end = Offset(x = size.width, y = size.height / 2f),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
         // draw y-axis
@@ -72,7 +79,7 @@ fun CanvasChartStepFour() {
             color = Color.Black,
             start = Offset(x = size.width / 2f, y = 0f),
             end = Offset(x = size.width / 2f, y = size.height),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
         val points = listOf(
@@ -129,13 +136,15 @@ fun CanvasChartStepFour() {
 @Preview(showBackground = true)
 @Composable
 fun CanvasChartStepFive() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
         // draw x-axis
         drawLine(
             color = Color.Black,
             start = Offset(x = 0f, y = size.height / 2f),
             end = Offset(x = size.width, y = size.height / 2f),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
         // draw y-axis
@@ -143,19 +152,7 @@ fun CanvasChartStepFive() {
             color = Color.Black,
             start = Offset(x = size.width / 2f, y = 0f),
             end = Offset(x = size.width / 2f, y = size.height),
-            strokeWidth = 4.dp.value,
-        )
-
-        val points = listOf(
-            Pair(-10f, 3f),
-            Pair(-8f, 5f),
-            Pair(-4f, -5f),
-            Pair(1f, 2f),
-            Pair(2f, 7f),
-            Pair(3f, 5f),
-            Pair(6f, -3f),
-            Pair(8f, 9f),
-            Pair(10f, 0f),
+            strokeWidth = lineWidth,
         )
 
         val xRangeMin = -10f
@@ -195,11 +192,23 @@ fun CanvasChartStepFive() {
                             x = calculateXCoordinate(nextPoint.first),
                             y = calculateYCoordinate(nextPoint.second),
                         ),
-                        strokeWidth = 4.dp.toPx(),
+                        strokeWidth = lineWidth,
                     )
                 }
             }
         }
+
+        val points1 = listOf(
+            Pair(-10f, 3f),
+            Pair(-8f, 5f),
+            Pair(-4f, -5f),
+            Pair(1f, 2f),
+            Pair(2f, 7f),
+            Pair(3f, 5f),
+            Pair(6f, -3f),
+            Pair(8f, 9f),
+            Pair(10f, 0f),
+        )
 
         val points2 = listOf(
             Pair(-10f, 1f),
@@ -213,21 +222,23 @@ fun CanvasChartStepFive() {
             Pair(10f, -2f),
         )
 
-        drawPoints(points, Color.Blue)
-        drawPoints(points2, Color.Green)
+        drawPoints(points1, Color.Blue)
+        drawPoints(points2, Color.Cyan)
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CanvasChartStepSix() {
-    Canvas(modifier = Modifier.size(400.dp)) {
+fun GradientSample() {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
         // draw x-axis
         drawLine(
             color = Color.Black,
             start = Offset(x = 0f, y = size.height / 2f),
             end = Offset(x = size.width, y = size.height / 2f),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
         // draw y-axis
@@ -235,10 +246,53 @@ fun CanvasChartStepSix() {
             color = Color.Black,
             start = Offset(x = size.width / 2f, y = 0f),
             end = Offset(x = size.width / 2f, y = size.height),
-            strokeWidth = 4.dp.value,
+            strokeWidth = lineWidth,
         )
 
-        val points = listOf(
+        val xRangeMin = -10f
+        val xRangeMax = 10f
+        val xAxisWidth = size.width
+        val xStep = xAxisWidth / (xRangeMax - xRangeMin)
+
+        val yRangeMin = -10f
+        val yRangeMax = 10f
+        val yAxisHeight = size.height
+        val yStep = yAxisHeight / (yRangeMax - yRangeMin)
+
+        val calculateXCoordinate: (x: Float) -> Float = { x ->
+            (x - xRangeMin) * xStep
+        }
+
+        val calculateYCoordinate: (y: Float) -> Float = { y ->
+            (yRangeMax - y) * yStep
+        }
+
+        val drawPoints: (
+            points: List<Pair<Float, Float>>,
+            color: Color,
+        ) -> Unit = { points, color ->
+
+            points.forEachIndexed { index, point ->
+                val nextPoint = points.getOrNull(index + 1)
+
+                nextPoint?.let {
+                    drawLine(
+                        color = color,
+                        start = Offset(
+                            x = calculateXCoordinate(point.first),
+                            y = calculateYCoordinate(point.second),
+                        ),
+                        end = Offset(
+                            x = calculateXCoordinate(nextPoint.first),
+                            y = calculateYCoordinate(nextPoint.second),
+                        ),
+                        strokeWidth = lineWidth,
+                    )
+                }
+            }
+        }
+
+        val points1 = listOf(
             Pair(-10f, 3f),
             Pair(-8f, 5f),
             Pair(-4f, -5f),
@@ -248,6 +302,45 @@ fun CanvasChartStepSix() {
             Pair(6f, -3f),
             Pair(8f, 9f),
             Pair(10f, 0f),
+        )
+
+        val points2 = listOf(
+            Pair(-10f, 1f),
+            Pair(-8f, 8f),
+            Pair(-4f, -5f),
+            Pair(1f, 3f),
+            Pair(2f, 5f),
+            Pair(3f, -2f),
+            Pair(6f, 8f),
+            Pair(8f, 1f),
+            Pair(10f, -2f),
+        )
+
+        drawPoints(points1, Color.Blue)
+        drawPoints(points2, Color.Cyan)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuadraticSample() {
+    Canvas(modifier = Modifier.size(400.dp).background(Color.LightGray)) {
+        val lineWidth = 4.dp.value
+
+        // draw x-axis
+        drawLine(
+            color = Color.Black,
+            start = Offset(x = 0f, y = size.height / 2f),
+            end = Offset(x = size.width, y = size.height / 2f),
+            strokeWidth = lineWidth,
+        )
+
+        // draw y-axis
+        drawLine(
+            color = Color.Black,
+            start = Offset(x = size.width / 2f, y = 0f),
+            end = Offset(x = size.width / 2f, y = size.height),
+            strokeWidth = lineWidth,
         )
 
         val xRangeMin = -10f
@@ -279,25 +372,37 @@ fun CanvasChartStepSix() {
                 calculateXCoordinate(firstPoint.first),
                 calculateYCoordinate(firstPoint.second),
             )
- 
+
             points.forEachIndexed { index, point ->
                 val nextPoint = points.getOrNull(index + 1)
 
                 nextPoint?.let {
                     linePath.cubicFromTo(
-                        xCord1 = calculateXCoordinate(point.first),
-                        yCord1 = calculateYCoordinate(point.first),
-                        xCord2 = calculateXCoordinate(point.second),
-                        yCord2 = calculateYCoordinate(point.second),
+                        point1x = calculateXCoordinate(point.first),
+                        point1y = calculateYCoordinate(point.second),
+                        point2x = calculateXCoordinate(nextPoint.first),
+                        point2y = calculateYCoordinate(nextPoint.second),
                     )
                 }
             }
             drawPath(
                 linePath,
                 color,
-                style = Stroke(width = 4.dp.toPx()),
+                style = Stroke(width = lineWidth),
             )
         }
+
+        val points1 = listOf(
+            Pair(-10f, 3f),
+            Pair(-8f, 5f),
+            Pair(-4f, -5f),
+            Pair(1f, 2f),
+            Pair(2f, 7f),
+            Pair(3f, 5f),
+            Pair(6f, -3f),
+            Pair(8f, 9f),
+            Pair(10f, 0f),
+        )
 
         val points2 = listOf(
             Pair(-10f, 1f),
@@ -311,32 +416,29 @@ fun CanvasChartStepSix() {
             Pair(10f, -2f),
         )
 
-        drawPoints(points, Color.Blue)
-        drawPoints(points2, Color.Green)
+        drawPoints(points1, Color.Blue)
+        drawPoints(points2, Color.Cyan)
     }
 }
 
 /**
  * Adds a cubic line to path from [point1] to [point2]
  */
-fun Path.cubicFromTo(xCord1: Float, yCord1: Float, xCord2: Float, yCord2: Float) {
+fun Path.cubicFromTo(point1x: Float, point1y: Float, point2x: Float, point2y: Float) {
     /**
-     * I don't really understand what this function with these params does...
-     * But I found it in this article ...
      * https://medium.com/@pranjalg2308/understanding-bezier-curve-in-android-and-moddinggraphview-library-a9b1f0f95cd0
-     * ... and it works very nicely.
      */
-    val conX1 = (xCord1 + xCord2) / 2f
-    val conY1 = yCord1
-    val conX2 = (xCord1 + xCord2) / 2f
-    val conY2 = yCord2
+    val conX1 = (point1x + point2x) / 2f
+    val conY1 = point1y
+    val conX2 = (point1x + point2x) / 2f
+    val conY2 = point2y
 
     cubicTo(
         conX1,
         conY1,
         conX2,
         conY2,
-        xCord2,
-        yCord2,
+        point2x,
+        point2y,
     )
 }
